5e6c093a37dfc73c69cfcb760c6d731221edaec9
Christopher Woodall <woodall.christopher@gmail.com>
2022-12-27 17:30:33 -0500

    Update main.js

--- babble/babble/www/assets/js/main.js
+++ babble/babble/www/assets/js/main.js
@@ -16,20 +16,22 @@ document.querySelector("form").onsubmit = (event) => {
     output_element.value = "";
 
     query_element.addEventListener("keyup", (event) => {
-      let query_text = encodeURIComponent(event.target.value.trim());
+      let query_text = event.target.value.trim();
 
       if(query_text.length > 4) {
         console.log(`Key pressed: ${event.key}`);
         console.log(`Query text: ${query_text}`);
+        let query_params = `query: ${encodeURIComponent(query_text)}`;
 
-        fetch(`http://127.0.0.1:9900/?text=${query_text}`)
+        fetch(`http://127.0.0.1:9900/?text=${query_params}`)
           .then( response => response.json() )
           .then( response => {
             console.log(response);
             console.log(response.generation);
             response.generation.forEach((res) => {
               console.log(res);
-              output_element.value = res['generated_text'] + output_element.value;
+              answers = res['generated_text'].split('event.dataset:smb_mapping | ').at(-1)
+              output_element.value = `${answers}\n${output_element.value}`;
             });
           });
       }
