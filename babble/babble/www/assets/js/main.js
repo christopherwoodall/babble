edef20b5934172d7ef2a5ab9b2366f46c100f326
Christopher Woodall <woodall.christopher@gmail.com>
2022-12-27 15:54:53 -0500

    update web interface

--- babble/babble/www/assets/js/main.js
+++ babble/babble/www/assets/js/main.js
@@ -1,10 +1,36 @@
+// 'use strict';
 
-
-document.querySelector('form').onsubmit = function(e){
-  e.preventDefault();
-
-  let selection = document.querySelector('select').selectedIndex;
+document.querySelector("form").onsubmit = (event) => {
+  event.preventDefault();
+  let selection = document.querySelector("select").selectedIndex;
   console.log(selection);
-
 };
 
+
+(() => {
+  window.addEventListener("load", () => {
+    let query_element = document.getElementById("query_text");
+    let output_element = document.getElementById("output");
+
+    query_element.addEventListener("keyup", (event) => {
+      let query_text = event.target.value.trim()
+
+      if(query_text.length > 4) {
+        console.log(`Key pressed: ${event.key}`);
+        console.log(`Query text: ${query_text}`);
+
+        fetch(`http://127.0.0.1:9900/?text=${query_text}`)
+          .then( response => response.json() )
+          .then( response => {
+            console.log(response);
+            console.log(response.generation);
+            response.generation.forEach((res) => {
+              console.log(res);
+            });
+          });
+      }
+    });
+  });
+})();
+
+void 0;
