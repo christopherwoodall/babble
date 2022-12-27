2519a8a5138a8c6c5988f67750101aa7cc9e33e5
Christopher Woodall <woodall.christopher@gmail.com>
2022-12-27 18:02:27 -0500

    Update main.js

--- babble/babble/www/assets/js/main.js
+++ babble/babble/www/assets/js/main.js
@@ -1,5 +1,20 @@
 // 'use strict';
 
+
+let parse_response = (query_text, response) => {
+  let responses = response.split(query_text).at(-1);
+
+  responses = response.split("\n").map((res) => {
+    if(res.toLowerCase().startsWith("query:")) {
+      return res.slice(6).trim();
+    }
+  });
+  responses = responses.filter((res) => { return res });
+
+  return responses.join("---\n");
+};
+
+
 document.querySelector("form").onsubmit = (event) => {
   event.preventDefault();
   let selection = document.querySelector("select").selectedIndex;
@@ -12,7 +27,7 @@ document.querySelector("form").onsubmit = (event) => {
     let query_element = document.getElementById("query_text");
     let output_element = document.getElementById("output");
 
-    query_element.value = "";
+    // query_element.value = "";
     output_element.value = "";
 
     query_element.addEventListener("keyup", (event) => {
@@ -23,17 +38,17 @@ document.querySelector("form").onsubmit = (event) => {
         console.log(`Query text: ${query_text}`);
         let query_params = `query: ${encodeURIComponent(query_text)}`;
 
-        fetch(`http://127.0.0.1:9900/?text=${query_params}`)
-          .then( response => response.json() )
-          .then( response => {
-            console.log(response);
-            console.log(response.generation);
-            response.generation.forEach((res) => {
-              console.log(res);
-              answers = res['generated_text'].split('event.dataset:smb_mapping | ').at(-1)
-              output_element.value = `${answers}\n${output_element.value}`;
+          fetch(`http://127.0.0.1:9900/?text=${query_params}`)
+            .then( response => response.json() )
+            .then( response => {
+              console.log(response);
+              console.log(response.generation);
+              response.generation.forEach((res) => {
+                console.log(res);
+                answers = parse_response(query_text, res['generated_text']);
+                output_element.value = `${answers}\n${output_element.value}`;
+              });
             });
-          });
       }
     });
   });
