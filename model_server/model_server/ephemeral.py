21a7fbd0a2671f3f4b104f25e01901f9273f6c59
Christopher Woodall <woodall.christopher@gmail.com>
2022-12-26 22:25:09 -0500

    Update ephemeral.py

--- model_server/model_server/ephemeral.py
+++ model_server/model_server/ephemeral.py
@@ -6,18 +6,18 @@ from . import server
 
 
 def input_loop():
-  while (in := input()) != 'q':
-    print(f'You typed: {in}')
-    # TODO: Send to server
+  while (user_input := input("QUERY: ")) != 'q':
+    print(f'You typed: {user_input}')
+    generation = server.autocomplete(user_input)
+    print(f'Generation: {generation}')
 
 
 def main():
-  print('Hello, ephemeral world!')
+  print('Hello world!')
   input_thread = Thread(target=input_loop, args=())
   input_thread.start()
   server.serve()
   input_thread.join()
-
 
 
 if __name__ == '__main__':
