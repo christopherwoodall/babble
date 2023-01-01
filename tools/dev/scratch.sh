c8db2551e603d2830f974ea42e4f88960f380cf1
Christopher Woodall <woodall.christopher@gmail.com>
2023-01-01 01:28:46 -0500

    Create scratch.sh

---
+++ tools/dev/scratch.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# Configure splunk with yaml.
+# https://splunk.github.io/docker-splunk/ADVANCED.html#runtime-configuration
+# docker run --rm -it splunk/splunk:latest create-defaults > default.yml
