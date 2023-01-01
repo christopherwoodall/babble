0d199fb34dbfc6cb5046df4a2c3bbda6bb1c4bf7
Christopher Woodall <woodall.christopher@gmail.com>
2022-12-31 23:45:34 -0500

    Update zeek.process.sh

--- tools/bin/zeek.process.sh
+++ tools/bin/zeek.process.sh
@@ -1,53 +1,60 @@
 #!/usr/bin/env bash
 
-# Check if git is installed
-if ! command -v git &> /dev/null; then
-    echo "git could not be found"
-    exit 1
+# Check if both git and docker are installed
+if [[ ! -x "$(command -v docker)" ]]; then
+  echo "Docker is not installed. Please install docker and try again."
+  exit 1
+fi
+if [[ ! -x "$(command -v git)" ]]; then
+  echo "Git is not installed. Please install git and try again."
+  exit 1
 fi
 
-# Fixes Github Action error, "tput: No value for $TERM and no -T specified",
-# that occurs When $TERM is empty (non-interactive shell) by faking a value
-# for the terminal profile.
-[[ ${TERM}=="" ]] && export TERM='xterm-256color'
 
 PROJECT_ROOT=`git rev-parse --show-toplevel`
 ZEEK_DOCKER_IMAGE="zeekurity/zeek:latest"
 ZEEK_DIR="data/ingest/zeek"
 PCAP_DIR="data/ingest/pcap"
 
+# Fixes Github Action error, "tput: No value for $TERM and no -T specified",
+[[ ${TERM}=="" ]] && export TERM='xterm-256color'
 
-pushd $PROJECT_ROOT
-  # Check if the zeek image is available locally. If not,
-  # pull the image from the docker registry.
-  if ! docker image inspect $ZEEK_DOCKER_IMAGE > /dev/null 2>&1; then
-    echo "Zeek image not found. Pulling..."
-    docker pull $ZEEK_DOCKER_IMAGE
-  fi
 
-  if [[ -z "${1}" ]]; then
-    echo "No pcap file specified. Running Zeek on all pcaps in $PCAP_DIR"
-    PCAP_FILES=`find $PCAP_DIR -type f -name "*.pcap"`
-    for PCAP_FILE in $PCAP_FILES; do
-      $0 $PCAP_FILE
-    done
-  else
+# Check if the zeek image is available locally. If not,
+# pull the image from the docker registry.
+if ! docker image inspect $ZEEK_DOCKER_IMAGE > /dev/null 2>&1; then
+  echo "Zeek image not found. Pulling..."
+  docker pull $ZEEK_DOCKER_IMAGE
+fi
+
+
+if [[ -z "${1}" ]]; then
+  echo "No pcap file specified. Running Zeek on all pcaps in $PCAP_DIR"
+  PCAP_FILES=`find $PCAP_DIR -type f -name "*.pcap"`
+  for PCAP_FILE in $PCAP_FILES; do
+    $0 $PCAP_FILE
+  done
+else
+  pushd $PROJECT_ROOT
     PCAP_FILE=$1
+    FILENAME=`basename $PCAP_FILE`
+    OUTPUT_DIR="${ZEEK_DIR}/`echo $FILENAME | sed 's/\.pcap$//g'`"
+
     echo "Running Zeek on ${PCAP_FILE}"
 
+    mkdir -p $OUTPUT_DIR
     docker run                     \
       --rm                         \
       --volume `pwd`/data:/data:z  \
-      --workdir /$ZEEK_DIR         \
+      --workdir /$OUTPUT_DIR       \
       $ZEEK_DOCKER_IMAGE           \
       zeek                         \
         --no-checksums             \
         --readfile /${PCAP_FILE}   \
         LogAscii::use_json=T       \
         local
-  fi
-popd
-
+  popd
+fi
 
 
 # NOTES:
