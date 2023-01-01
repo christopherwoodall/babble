bf2da9b20741287a8e8279f7faa3e470b36cbb0c
Christopher Woodall <woodall.christopher@gmail.com>
2023-01-01 01:29:57 -0500

    remove tool directory

--- tools/dev/scratch.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# Configure splunk with yaml.
-# https://splunk.github.io/docker-splunk/ADVANCED.html#runtime-configuration
-# docker run --rm -it splunk/splunk:latest create-defaults > default.yml
